10903f2a28a734e3bbf9929867937edea461fd62
Isak_Almquist <isaal46@liu.se>
2020-03-10 11:40:11 +0100

    created working main

--- server/main.py
+++ server/main.py
@@ -1,9 +1,9 @@
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../client', static_url_path='/')
 
 @app.route("/")
-def client():
+def client(): 
     return app.send_static_file("client.html")
 
 if __name__ == "__main__":

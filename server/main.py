8ccef9793b5f49dfb7452a213a1b5c3a90feb55f
Isak Almquist <isaal360@student.liu.se>
2020-03-12 10:55:30 +0100

    Fixed errors in main

--- server/main.py
+++ server/main.py
@@ -1,33 +1,45 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import DateTime
 from datetime import datetime
 
 app = Flask(__name__, static_folder='../client', static_url_path='/')
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://database.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-class Activity(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    date = db.Column(DateTime)
-    name = db.Column(db.String, nullable = False)
-    startTime = db.Column(DateTime)
-    stopTime = db.Column(DateTime)
-    location = db.Column(db.String, nullable = True)
-    description = db.Column(db.String, nullable = True)
-    project = db.Column(db.Integer, db.ForeignKey('Project.id'))
+class Project(db.Model):
+    __tablename__ = 'projects'
+    id=db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    #activities = db.relationship('Activity', backref='project', lazy=True)
 
     def __repr__(self):
-        return '<Activity {} : {} {} {} {} {} {} {} {}>'.format(
-            id, self.date, self.name, self.startTime, self.stopTime, self.location, self.description, self.project)
+        return '<Project {}: {}>'.format(self.id, self.name)
+
+    def serialize(self):
+        return dict(id=self.id, name=self.name)
+
+class Activity(db.Model):
+    __tablename__ = 'activities'
+    id = db.Column(db.Integer, primary_key = True)
+    date = db.Column(db.DateTime)
+    name = db.Column(db.String, nullable = False)
+    startTime = db.Column(db.DateTime)
+    stopTime = db.Column(db.DateTime)
+    location = db.Column(db.String, nullable = True)
+    description = db.Column(db.String, nullable = True)
+    project_id = db.Column(db.Integer, db.ForeignKey('projects.id'), nullable = False)
+
+    def __repr__(self):
+        return '<Activity {} : {} {} {} {} {} {} {}>'.format(self.id, self.date, self.name, self.startTime, self.stopTime, self.location, self.description, self.project)
     def serialize(self):
         return dict(id = self.id, date = self.date, name = self.name, startTime = self.startTime,
         stopTime = self.StopTime, location = self.Location, description = self.description, project = self.project)
 
 class Employee(db.Model):
+    __tablename__ = 'employees'
     personID = db.Column(db.String, primary_key=True)
     name = db.Column(db.String, nullable=False)
     isAdmin = db.Column(db.Boolean, nullable=False)
@@ -40,19 +52,10 @@ class Employee(db.Model):
         return dict(personID=self.personID, name=self.name, isAdmin=self.isAdmin, isBoss=self.isBoss)
 
 
-class Project(db.Model):
-    id=db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False)
-
-    def __repr__(self):
-        return '<Project {}: {}>'.format(self.id, self.name)
-
-    def serialize(self):
-        return dict(id=self.id, name=self.name)
-
 class Person_Activity(db.Model):
-    personId = db.Column(db.String, db.ForeignKey('Employee.personID'), primary_key=True)
-    id = db.Column(db.Integer, db.ForeignKey('Activity.id'), primary_key=True)
+    __tablename__ = 'person_activities'
+    personId = db.Column(db.String, db.ForeignKey('employees.personID'), primary_key=True)
+    id = db.Column(db.Integer, db.ForeignKey('activities.id'), primary_key=True)
 
     def __repr__(self):
         return '<Person_Activity {} {}:>'.format(self.personID, self.id)
@@ -61,11 +64,12 @@ class Person_Activity(db.Model):
         return dict(personID=self.personID, id=self.id)
 
 class LoggedWork(db.Model):
+    __tablename__ = 'logged_work'
     id = db.Column(db.Integer, primary_key=True)
     employeeID = db.Column(db.Integer, db.ForeignKey(
-        'Employee.id'), nullable=False)
+        'employees.personID'), nullable=False)
     projectID = db.Column(db.Integer, db.ForeignKey(
-        'Project.id'), nullable=False)
+        'projects.id'), nullable=False)
     approved = db.Column(db.Boolean, default=True)
     startTime = db.Column(db.Integer, nullable=False)
     endTime = db.Column(db.Integer, nullable=False)
@@ -86,4 +90,5 @@ def client():
 
 
 if __name__ == "__main__":
+    db.create_all()
     app.run()

a566dfeb5cb251197889d26a0a5f7f8bd2dcdda1
Isak Almquist <28620471+isakalmq@users.noreply.github.com>
2020-04-02 11:21:45 +0200

    added view change

--- client/client.js
+++ client/client.js
@@ -1,25 +1,9 @@
 function changeToCalendarView() {
     $("#mainView").html($("#calendarView").html())
-        var calendarEl = document.getElementById('calendar');
-
-        var calendar = new FullCalendar.Calendar(calendarEl, {
-            timeZone: 'local',
-            plugins: [ 'timeGrid' ],
-            defaultView: 'timeGridDay',
-            events: [
-              {
-                  title: 'Titeln',
-                  start: '2020-04-01T12:30:00',
-                  end: '2020-04-01T13:30:00'
-
-              }
-            ]
-        });
-
-        
-        calendar.render();
+    createCalendar();
 }
 
 $( document ).ready(function() {
     $("#mainView").html($("#landingPage").html())
+    changeToCalendarView()
 })

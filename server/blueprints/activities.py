5070fc13dc88b829cb9396222a1bb214e6242a0a
Isak Almquist <28620471+isakalmq@users.noreply.github.com>
2020-04-03 17:13:13 +0200

    Removed redundant function

--- server/blueprints/activities.py
+++ server/blueprints/activities.py
@@ -11,23 +11,7 @@ bp = Blueprint('activities', __name__, url_prefix='/activity')
 
 
 @bp.route("/add", methods=['POST'])
-
-# Dates should be send in iso formating, YYYY-MM-DD.
-# Example:
-# localhost/activity/2020-01-04
-@bp.route("/<date>", methods=['GET'])
-def getActivitiesOnDate(date):
-    try:
-        convertedDate = d.fromisoformat(date)
-    except:
-        return {"msg": "wrong formated date"}, 400
-    activities = db.session.query(Activity).filter(
-        Activity.date == convertedDate).all()
-
-    serialized_activities = [activity.serializeForCalendar()
-                             for activity in activities]
-    return jsonify(serialized_activities), 200
-
+def addActivity():
 
 # Returns the activities in the given timeframe. Only the users activities are returned.
 # The url format for accessing this is: localhost:5000/feed?start=TIME-IN-ISO-FORMAT&end=TIME-IN-ISO-FORMAT
@@ -41,8 +25,7 @@ def activityFeed():
     user_id = get_jwt_identity()
 
     activities = db.session.query(Activity).\
-        join(Person_Activity).\
-        filter(Activity.id == Person_Activity.id).\
+        join(Person_Activity, Person_Activity.id == Activity.id).\
         filter(Person_Activity.personID == user_id).\
         filter(Activity.startTime >= start).\
         filter(Activity.startTime <= end).\

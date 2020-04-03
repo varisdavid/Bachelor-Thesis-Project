a71aa87cf65398b9cd2a3638b78ab8f3a15cc5f3
Isak Almquist <28620471+isakalmq@users.noreply.github.com>
2020-04-03 12:57:51 +0200

    Created a 'feed' for fetching activities from the server. Also implemented front end that uses said feed

--- client/calendar.js
+++ client/calendar.js
@@ -3,21 +3,28 @@
  */
 var calendar;
 
+/**
+ * Creates the calendar, needs a html element with a 'calendar' id for it to work.
+ * Fetches only the data needed from the server.
+ */
 function createCalendar() {
     var calendarEl = document.getElementById('calendar');
-    $.ajax({
-        url: '/activity/current_user',
-        type: 'GET',
-        headers: { "Authorization": "Bearer " + JSON.parse(sessionStorage.getItem('auth')).access_token},
-        success: function (response) {
-            calendar = new FullCalendar.Calendar(calendarEl, {
-                timeZone: 'local',
-                plugins: [ 'timeGrid' ],
-                defaultView: 'timeGridWeek',
-                events: response
-            });
-            calendar.render();
-        },
-    })
-}
 
+    calendar = new FullCalendar.Calendar(calendarEl, {
+        timeZone: 'local',
+        plugins: ['timeGrid'],
+        defaultView: 'timeGridWeek',
+        nowIndicator: true,
+        events: function (info, callback) {
+            $.ajax({
+                url: `/activity/feed?start=${info.startStr}&end=${info.endStr}`,
+                type: 'GET',
+                headers: { "Authorization": "Bearer " + JSON.parse(sessionStorage.getItem('auth')).access_token},
+                success: function (response) {
+                    callback(response);
+                },
+            });
+        }
+    });
+    calendar.render();
+}
--- server/blueprints/activities.py
+++ server/blueprints/activities.py
@@ -11,6 +11,7 @@ bp = Blueprint('activities', __name__, url_prefix='/activity')
 
 
 @bp.route("/add", methods=['POST'])
+
 # Dates should be send in iso formating, YYYY-MM-DD.
 # Example:
 # localhost/activity/2020-01-04
@@ -27,18 +28,24 @@ def getActivitiesOnDate(date):
                              for activity in activities]
     return jsonify(serialized_activities), 200
 
-# returns the activities of the current user formatted in fullcalender event parsable json.
-# i.e. the return values from this route can be passed directly into the fullcalendar events field
-# 
-# TODO: Should return a subset of all activities. For example only upcoming activities
-@bp.route("/current_user", methods=['GET'])
+
+# Returns the activities in the given timeframe. Only the users activities are returned.
+# The url format for accessing this is: localhost:5000/feed?start=TIME-IN-ISO-FORMAT&end=TIME-IN-ISO-FORMAT
+# An example url would look like: localhost:5000/feed?start=2020-03-29T00:00:00+01:00&end=2020-04-05T00:00:00+02:00
+#TODO: Implement some safety checks if necessary. Not sure.
+@bp.route("/feed")
 @jwt_required
-def getUserActivities():
+def activityFeed():
+    start = request.args.get("start")
+    end = request.args.get("end")
     user_id = get_jwt_identity()
+
     activities = db.session.query(Activity).\
         join(Person_Activity).\
         filter(Activity.id == Person_Activity.id).\
         filter(Person_Activity.personID == user_id).\
+        filter(Activity.startTime >= start).\
+        filter(Activity.startTime <= end).\
         all()
-    
+
     return jsonify([activity.serializeForCalendar() for activity in activities]), 200

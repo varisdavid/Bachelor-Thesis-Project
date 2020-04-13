50032607c0b1da238bf80fab1bcd95e607475f20
CJ mac <crippe.johansson@telia.com>
2020-04-13 17:39:36 +0200

    problem med authentication, allt annat löst

--- client/employee.js
+++ client/employee.js
@@ -1,8 +1,10 @@
 function loadEmployees() {
     $.ajax({
-        //headers: {"Authorization": "Bearer " + JSON.parse(sessionStorage.getItem('auth')).token },
+        headers: { "Authorization": "Bearer " + JSON.parse(sessionStorage.getItem('auth')).token },
         url: 'http://127.0.0.1:5000/employee/all',
         type: 'GET',
+        dataType: 'json',
+        contentType: 'application/json',
         success: function(employees) {
             console.log("EMPLOYEE/ALL SUCCESS");
             for (employee of employees) {
@@ -11,8 +13,9 @@ function loadEmployees() {
                     employee.name + "</td><td>" +
                     employee.isAdmin + "</td><td>" +
                     employee.isBoss + "</td><td>" +
-                    "<button id = 'book-button' class = 'btn btn-primary'> Booked by " + car.user.name + "</button>" +
-                    "<button id = 'unbook-button' class = 'btn btn-danger' onclick = 'unbook_car(" + car.id + ")'> Unbook </button>" +
+                    "</td><td>" +
+                    "</td><td>" +
+                    "<button id = 'book-button' class = 'btn btn-primary'> Redigera </button>" +
                     "</td></tr>");
             }
         }
--- server/blueprints/employee.py
+++ server/blueprints/employee.py
@@ -1,31 +1,44 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify, Response
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity
 
 # You can import the database from a blueprint
-from server.database import db, Employee
+from server.database import db, Employee, Company, Project
+from server import app
 
+# random string
+app.config['JWT_SECRET_KEY'] = "GwzrtfCta1xDHgwfBVo0"
+bcrypt = Bcrypt(app)
+jwt = JWTManager(app)
 
 # Creates the blueprint
 bp = Blueprint('employee', __name__, url_prefix='/employee')
 
 # Route for fetching the entire employee table and adding an employee
 @bp.route("/all", methods=['GET', 'POST'])
-#@jwt_required
+@jwt_required
 def employees():
-    #user_id = get_jwt_identity()
-    #employee = Employee.query.get(user_id)
+    user_id = get_jwt_identity()
+    print(user_id)
+    employee = Employee.query.get_or_404(user_id)
+    print(employee)
+    print(employee.company)
     if request.method == 'GET':
-        serializedEmployeeList = [i.serialize() for emp in Employee.query.all()]
+        print("==GET==")
+        serializedEmployeeList = [emp.serialize() for emp in Employee.query.all()]
         return jsonify(serializedEmployeeList)
     if request.method == 'POST':
-        #if employee.isAdmin:
-        jsonData = request.get_json()
-        db.session.add(Employee(personID=jsonData['personID'], email=jsonData['email'], name=jsonData['name'],
-                                isAdmin=False, isBoss=False, company=employee.company, passwordHash = bcrypt.generate_password_hash("halla").decode('utf-8'), usingDefaultPassword=True))
-        db.session.commit()
-        return 200
-        #Employee.query.get_or_404(jsonData['personID']).serialize(), 200
-        #else:
-        #return {"msg": "Not authorized"}, 401
+        print("==POST==")
+        if employee.isAdmin:
+            print("IS ADMIN")
+            jsonData = request.get_json()
+            db.session.add(Employee(personID=jsonData['personID'], email=jsonData['email'], name=jsonData['name'],
+                                    isAdmin=False, isBoss=False, company=employee.company, passwordHash = bcrypt.generate_password_hash("halla").decode('utf-8'), usingDefaultPassword=True))
+            db.session.commit()
+            return Employee.query.get_or_404(jsonData['personID']).serialize(), 200
+        else:
+            print("NOT ADMIN")
+            return {"msg": "Not authorized"}, 401
 
 # Route for fetsching one employee, editing one employee and removing one employee
 @bp.route("/<int:pID>", methods=['GET', 'PUT', 'DELETE'])
